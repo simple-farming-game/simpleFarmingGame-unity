3d9927bd60db46cc46772ac40dc2be3037536b73
newkincode <maainnewkin59@gmail.com>
2023-06-13 20:04:14 +0900

    add exit btn, font

---
+++ Assets/Script/ExitBtn.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonEvent : MonoBehaviour
+{
+    public GameObject button;
+
+    public void exitClick()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit(); // 어플리케이션 종료
+        #endif
+    }
+}
